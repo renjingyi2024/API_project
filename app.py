4ad755a493f82dfaa3a5d0b1507b8ad4f1ce126d
strawberryee <strawberryee@strawberryeedeMacBook-Pro.local>
2025-02-21 11:58:13 +0100

    Add InputPayload model with Pydantic

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class InputPayload(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+    

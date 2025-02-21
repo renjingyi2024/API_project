dc0dd9d624b2ad5b81ba356c5dd1f699210e7d9d
strawberryee <strawberryee@strawberryeedeMacBook-Pro.local>
2025-02-21 13:15:09 +0100

    Add model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pickle
 
 app = FastAPI()
 
@@ -8,4 +9,15 @@ class InputPayload(BaseModel):
     sepal_width: float
     petal_length: float
     petal_width: float
+
+with open("model.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
     
+@app.post("/echo")
+async def echo(payload: InputPayload):
+    return payload
+
+@app.get("/status")
+async def status():
+    return {"message": "API is running", "model_loaded": model is not None}
+
